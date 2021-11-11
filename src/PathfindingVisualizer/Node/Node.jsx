b3a212613708861a7f29d2d4a9a3a172dcdee6be
RAHUL <rahul.chauhan049@gmail.com>
2021-11-11 16:11:58 +0530

    Initialize project

---
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Node() {
+    return (
+        <div>
+            Bar
+        </div>
+    )
+}
+
+export default Node
---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Node from './Node/Node'
+function PathfindingVisualizer() {
+    return (
+        <div>
+            Foo
+            <Node></Node>
+        </div>
+    )
+}
+
+export default PathfindingVisualizer
