12b6e09bd0307361ad7f6b79b99b5249d59f23e7
RAHUL <rahul.chauhan049@gmail.com>
2021-11-23 19:35:18 +0530

    Created Merge Sort Visualiation and added tailwind and styled component

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,47 +1,124 @@
-import React, { useEffect } from 'react'
-import './SortingVisualizer.css'
+import React, { useEffect } from "react";
+import "./SortingVisualizer.css";
+import { getMergeSortAnimations } from "../algorithms/sorting/mergeSort";
+import Slider from "@mui/material/Slider";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
+import tw from "twin.macro"
 
 function SortingVisualizer() {
-    const [heightArray, setHeightArray] = React.useState([])
-    const [arrayLength, setArrayLength] = React.useState(90)
+  const [heightArray, setHeightArray] = React.useState([]);
+  const [arrayLength, setArrayLength] = React.useState(20);
 
-    useEffect(() => {
-        let arrayOfSizeN = Array(arrayLength).fill().map((v, i) => i+10);
-        let shuffledArrayOfSizeN = arrayOfSizeN
-            .map((value) => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
-        setHeightArray(shuffledArrayOfSizeN)
-    }, [arrayLength])
+  useEffect(() => {
+    let arrayOfSizeN = Array(arrayLength)
+      .fill()
+      .map((v, i) => i + 10);
+    let shuffledArrayOfSizeN = arrayOfSizeN
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+    setHeightArray(shuffledArrayOfSizeN);
+  }, [arrayLength]);
 
-    const shuffleArray = () => {
-        let arrayOfSizeN = Array(arrayLength).fill().map((v, i) => i+1);
-        let shuffledArrayOfSizeN = arrayOfSizeN
-            .map((value) => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
-        setHeightArray(shuffledArrayOfSizeN)
+  const shuffleArray = () => {
+    let arrayOfSizeN = Array(arrayLength)
+      .fill()
+      .map((v, i) => i + 1);
+    let shuffledArrayOfSizeN = arrayOfSizeN
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+    setHeightArray(shuffledArrayOfSizeN);
+  };
+
+  const sortArray = () => {
+    const animations = getMergeSortAnimations(heightArray);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? "red" : "#4286f4";
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * 10);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}%`;
+        }, i * 10);
+      }
     }
+  };
 
-    return (
-        <div className="app">
-            <input type="range" min="10" max="100" value={arrayLength} onChange={(e) => setArrayLength(Number(e.target.value))} />
-            <button onClick={() => shuffleArray()}>Random</button>
-            <div className="array-container">
-                {heightArray.map((height, index) => (
-                    <div
-                        className="array-bar"
-                        key={index}
-                        style={{
-                            width: `${0.9}vw`,
-                            backgroundColor: '#4286f4',
-                            height: `${height}vh`,
-                        }}></div>
-                ))}
-               
-            </div>
-        </div>
-    )
+  return (
+    <App>
+      <Toolbar>
+        <SliderBox>
+          <Slider
+            defaultValue={arrayLength}
+            aria-label="Default"
+            onChange={(e) => setArrayLength(Number(e.target.value))}
+            valueLabelDisplay="auto"
+          />
+       </SliderBox>
+        <RandomButton>
+          <Button variant="text" onClick={() => shuffleArray()}>
+            Random
+          </Button>
+        </RandomButton>
+        <MergeButton>
+          <Button variant="text" onClick={() => sortArray()}>
+            Merge Sort
+          </Button>
+        </MergeButton>
+      </Toolbar>
+          <Visualization>
+              <Height></Height>
+        {heightArray.map((height, index) => (
+          <Bar className="array-bar"
+            key={index}
+            style={{
+              width: `${0.5}vw`,
+              backgroundColor: "#4286f4",
+              height: `${height}%`,
+            }}
+          ></Bar>
+        ))}  
+      </Visualization>
+
+    </App>
+  );
 }
 
-export default SortingVisualizer
+export default SortingVisualizer;
+
+let App = styled.div`
+    ${tw`h-1 flex flex-direction[column] flex-auto`}
+`;
+
+let Toolbar = styled.div`
+${tw`flex justify-between items-center bg-yellow-50 `}
+`;
+let Visualization = styled.div`
+${tw`flex justify-around align-items[flex-end] flex-1 bg-red-50`}
+`;
+
+let SliderBox = styled.div`
+${tw`flex-1`}
+`;
+
+let RandomButton = styled.div``;
+let MergeButton = styled.div``;
+
+let Height = styled.div`
+${tw`h-screen`}
+`;
+
+let Bar = styled.div`
+${tw` mx-0.5 max-h-screen mb-20`}`;
