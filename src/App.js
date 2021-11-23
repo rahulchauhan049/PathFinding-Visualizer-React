c80cb600b097b6fcdd3ea880227ddf3f82dc2453
RAHUL <rahul.chauhan049@gmail.com>
2021-11-23 19:35:58 +0530

    Added Toggle to toggle between Pathfinding and sorting visualization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import './App.css';
-import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer.js'
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer.js";
+import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
+import styled from "styled-components";
+import tw from "twin.macro";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function App() {
+  const [view, setView] = useState("sorting");
+
+  const toggleVisualization = () => {
+    if (view === "sorting") {
+      setView("pathfinding");
+    } else {
+      setView("sorting");
+    }
+  };
+
   return (
-    <div className="App">
-      <SortingVisualizer></SortingVisualizer>
-      {/* <PathfindingVisualizer></PathfindingVisualizer> */}
-    </div>
+    <Container>
+      <ToggleButton>
+        <Button variant="text" onClick={() => toggleVisualization()}>
+          Switch to {view === "sorting" ? "Pathfinding" : "Sorting"} Visualization
+        </Button>
+      </ToggleButton>
+      <Visualizations>
+        {view === "sorting" && <SortingVisualizer></SortingVisualizer>} 
+        {view === "pathfinding" && <PathfindingVisualizer></PathfindingVisualizer>}
+      </Visualizations>
+    </Container>
   );
 }
 
 export default App;
+
+let Container = styled.div`
+  ${tw` `}
+`;
+
+let ToggleButton = styled.div`
+${tw`justify-center text-center`}
+`;
+
+let Visualizations = styled.div`
+${tw``}
+`;
